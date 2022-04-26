7e6672e89c3cd1ab24b4c5ecaff257d385c015c6
newcomet-lab <newcometcto729@gmail.com>
2022-04-26 11:59:07 +0900

    astroMoralis hook

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,18 @@
+import BN from 'bn.js';
+
+export const calcAPY = (b1, b2, b3, dec) => {
+    if (!b1) return null;
+    if (!b2) b2 = 10000000000;
+    if (!b3) b3 = 48;
+    if (!dec) dec = 2;
+
+    const b4 = new BN(b2 + b1);
+    const b5 = new BN(b2);
+    const b6 = new BN(b3);
+
+    return b4.pow(b6).mul(new BN(Math.pow(10, dec + 3))).div(b5.pow(b6)).sub(new BN(Math.pow(10, dec + 3)));
+}
+
 export function formatFloatFixed(val, dec) {
     if (!dec) dec = 2;
     if (Math.log10(val) > 4 && dec > 4) dec = 4;
--- src/views/dashboard/CommunityPerformance.js
+++ src/views/dashboard/CommunityPerformance.js
@@ -10,41 +10,11 @@ import {
 import MainCard from 'ui-component/cards/MainCard';
 import SubCard from 'ui-component/cards/SubCard';
 
-import { useApiContract } from "react-moralis";
-import ASTRO_ABI from '_common/astro-abi.json';
-import { astroTokenAddress } from '_common/address';
 import { formatFloatFixed } from 'utils/helpers';
-
-import BN from 'bn.js';
-
-const commonAstroApiObj = {
-    abi: ASTRO_ABI,
-    address: astroTokenAddress,
-    chain: 'avalanche',
-    params: {}
-};
-
-const rewardApiOpt = { ...commonAstroApiObj, functionName: "rewardYield" };
-const rewardDominatorApiOpt = { ...commonAstroApiObj, functionName: "rewardYieldDenominator" };
-const rebaseFrequencyApiOpt = { ...commonAstroApiObj, functionName: "rebaseFrequency" };
-
-const calcAPY = (b1, b2, b3, dec) => {
-    if (!b1) return null;
-    if (!b2) b2 = 10000000000;
-    if (!b3) b3 = 48;
-    if (!dec) dec = 2;
-
-    const b4 = new BN(b2 + b1);
-    const b5 = new BN(b2);
-    const b6 = new BN(b3);
-
-    return b4.pow(b6).mul(new BN(Math.pow(10, dec + 3))).div(b5.pow(b6)).sub(new BN(Math.pow(10, dec + 3)));
-}
+import useAstroMoralis from 'hooks/useAstroMoralis';
 
 export default function CommunityPerformance() {
-    const rewardApiObj = useApiContract(rewardApiOpt);
-    const rewardDominatorApi = useApiContract(rewardDominatorApiOpt);
-    const rebaseFrequencyApi = useApiContract(rebaseFrequencyApiOpt);
+    const [{ astroAPY, astroROI }] = useAstroMoralis();
 
     return (
         <MainCard title="COMMUNITY PERFORMANCE">
@@ -124,17 +94,8 @@ export default function CommunityPerformance() {
                                 whiteSpace: 'nowrap',
                                 marginBottom: '5px'
                             }}>{
-                                rewardApiObj && rewardApiObj.data && rewardDominatorApi && rewardDominatorApi.data && rebaseFrequencyApi && rebaseFrequencyApi.data ?
-                                    formatFloatFixed(
-                                        Math.round(
-                                            calcAPY(
-                                                Number(rewardApiObj.data),
-                                                Number(rewardDominatorApi.data),
-                                                365 * 24 * 3600 / Number(rebaseFrequencyApi.data), 2
-                                            ).toNumber() / 10
-                                        ) / 100
-                                        , 2
-                                    ) + '%' : <Skeleton variant="rectangular" width={'100%'} height={35} />
+                                astroAPY ? formatFloatFixed(astroAPY, 2) + '%'
+                                    : <Skeleton variant="rectangular" width={'100%'} height={35} />
                             }</Typography>
                             <Typography sx={{
                                 color: 'hsla(0,0%,100%,.8)',
@@ -143,17 +104,8 @@ export default function CommunityPerformance() {
                                 margin: 0,
                                 textAlign: 'left',
                             }}>Daily % Rate (DPR): {
-                                rewardApiObj && rewardApiObj.data && rewardDominatorApi && rewardDominatorApi.data && rebaseFrequencyApi && rebaseFrequencyApi.data ?
-                                    formatFloatFixed(
-                                        Math.round(
-                                            calcAPY(
-                                                Number(rewardApiObj.data),
-                                                Number(rewardDominatorApi.data),
-                                                24 * 3600 / Number(rebaseFrequencyApi.data), 2
-                                            ).toNumber() / 10
-                                        ) / 100
-                                        , 2
-                                    ) + '%' : <Skeleton variant="rectangular" width={'100%'} height={24} />
+                                astroROI ? formatFloatFixed(astroROI, 2) + '%'
+                                    : <Skeleton variant="rectangular" width={'100%'} height={24} />
                             }</Typography>
                         </SubCard>
                     </Grid>
