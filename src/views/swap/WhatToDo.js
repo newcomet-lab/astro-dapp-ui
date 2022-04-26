d0aeb2190dd9bd325ca8615c8053b42c37f5f0b2
newcomet-lab <newcometcto729@gmail.com>
2022-04-27 05:54:26 +0900

    updated slider of dayCounter

--- src/views/swap/WhatToDo.js
+++ src/views/swap/WhatToDo.js
@@ -12,6 +12,7 @@ import MainCard from 'ui-component/cards/MainCard';
 import SubCard from 'ui-component/cards/SubCard';
 
 export default function WhatToDo() {
+    const [dayCounter, setDayCounter] = React.useState(30);
 
     return (
         <MainCard title="WHAT TO DO">
@@ -304,11 +305,31 @@ export default function WhatToDo() {
                         <Typography sx={{
                             fontSize: '14px',
                             fontFamily: 'Poppins',
-                        }}>159 days</Typography>
+                        }}>{dayCounter} days</Typography>
                         <Slider
                             aria-label="Temperature"
-                            defaultValue={30}
-
+                            min={1}
+                            max={365}
+                            step={1}
+                            value={dayCounter}
+                            onChange={(e, val) => {setDayCounter(val);}}
+                            sx={{
+                                '& .MuiSlider-thumb': {
+                                    background: '#1976d1',
+                                    border: '3px solid #7a1bff',
+                                    height: '28px',
+                                    width: '28px',
+                                },
+                                height: '12px',
+                                '& .MuiSlider-track': {
+                                    border: '1px solid #1976d1',
+                                    background: 'linear-gradient(180deg,#7929ff,#5c44ff)'
+                                },
+                                '& .MuiSlider-rail': {
+                                    border: '2px solid #5c44ff',
+                                    background: '#113c70',
+                                },
+                            }}
                         />
                     </Grid>
                     <Grid item xs={12} sm={12} sx={{ padding: '0px 16px', display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
